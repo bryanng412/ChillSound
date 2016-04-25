069a7421c79563dcbd59b1296cbb8f63a0e621cc
bryanng412 <bng@ucdavis.edu>
2016-04-25 16:19:24 -0700

    update root page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'static_pages#root'
 
   namespace :api, defaults: { format: :json } do
-    resource :user
+    resources :user, only: [ :create, :new, :show, :update ]
   end
 end
