f55659bc528b77d21d47c0831f950269a734f916
bryanng412 <bng@ucdavis.edu>
2016-04-25 15:48:38 -0700

    typo in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@
 #
 
 class User < ActiveRecord::Base
-  validates :username, presence: true, uniqueness: trueB
+  validates :username, presence: true, uniqueness: true
   validates :password_digest, :session_token, presence: true
   validates :password, length: { minimum: 6, allow_nil: true }
 
