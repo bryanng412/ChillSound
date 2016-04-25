e06c643bf1b223253f89004af970fffdefdcacd9
bryanng412 <bng@ucdavis.edu>
2016-04-25 11:49:24 -0700

    implement user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,6 +34,12 @@ class User < ActiveRecord::Base
     BCrypt::Password.new(self.password_digest).is_password?(password)
   end
 
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64(16)
+    self.save!
+    self.session_token
+  end
+
   private
   def ensure_session_token
     self.session_token ||= SecureRandom.urlsafe_base64(16)
