03256618388cd05a96b1dab647f82f3cae035d89
bryanng412 <bng@ucdavis.edu>
2016-05-04 17:30:53 -0700

    implement user profile page

---
+++ frontend/components/nav_song_item.jsx
@@ -0,0 +1,48 @@
+var React = require('react');
+var ClientActions = require('../actions/client_actions.js');
+var PlayerActions = require('../actions/player_actions.js');
+var Glyphicon = require('react-bootstrap').Glyphicon;
+var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
+
+var NavSongItem = React.createClass({
+  playSong: function(e) {
+    e.preventDefault();
+    ClientActions.increasePlayCount(this.props.song);
+    this.props.song.plays = parseInt(this.props.song.plays) + 1;
+    PlayerActions.playSong(this.props.song);
+  },
+
+  addToQueue: function(e) {
+    e.preventDefault();
+    PlayerActions.addToQueue(this.props.song);
+  },
+
+  render: function() {
+    return (
+      <ReactCSSTransitionGroup
+        transitionName="scroll"
+        transitionAppearTimeout={500}
+        transitionEnterTimeout={500}
+        transitionLeaveTimeout={500}
+        transitionAppear={true}
+      >
+        <figure className="songIndexItem">
+          <img src={this.props.song.image_url}/>
+          <figcaption>
+            <h3>{this.props.song.title}</h3>
+            <p>{this.props.song.artist}</p>
+            <div className="playIcon">
+              <Glyphicon onClick={this.playSong} glyph="play-circle" />
+            </div>
+            <div id="userPlusIcon" className="icons">
+              <Glyphicon onClick={this.addToQueue} glyph="plus" />
+            </div>
+          </figcaption>
+        </figure>
+      </ReactCSSTransitionGroup>
+    );
+  }
+
+});
+
+module.exports = NavSongItem;
--- frontend/components/user_nav.jsx
+++ frontend/components/user_nav.jsx
@@ -2,8 +2,10 @@ var React = require('react');
 var Navbar = require('react-bootstrap').Navbar;
 var Nav = require('react-bootstrap').Nav;
 var NavItem = require('react-bootstrap').NavItem;
+var NavSongItem = require('./nav_song_item.jsx');
 var LinkedStateMixin = require('react-addons-linked-state-mixin');
 var CurrentUserState = require('../mixins/current_user_state.js');
+var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
 
 var UserNav = React.createClass({
   mixins: [LinkedStateMixin, CurrentUserState],
@@ -18,16 +20,33 @@ var UserNav = React.createClass({
   },
 
   render: function() {
-    // var songs = this.state.activeKey === 1 ?
-    //
-    // {songs}
+    if (this.state.currentUser){
+      var songs = this.state.activeKey === 1 ?
+      this.state.currentUser.likedSongs.map(function(song, i){
+        return <NavSongItem key={i} song={song} />;
+      })
+      :
+      this.state.currentUser.songs.map(function(song, i){
+        return <NavSongItem key={i} song={song} />;
+      });
+    }
+
     return (
-      <div>
+      <ReactCSSTransitionGroup
+        transitionName="scroll"
+        transitionAppearTimeout={500}
+        transitionEnterTimeout={500}
+        transitionLeaveTimeout={500}
+        transitionAppear={true}
+      >
         <Nav bsStyle="tabs" activeKey={this.state.activeKey} onSelect={this.handleSelect}>
           <NavItem eventKey={1}>Likes</NavItem>
           <NavItem eventKey={2}>Uploaded Songs</NavItem>
         </Nav>
-      </div>
+        <div className="userSongIndex">
+          {songs}
+        </div>
+      </ReactCSSTransitionGroup>
     );
   }
 
