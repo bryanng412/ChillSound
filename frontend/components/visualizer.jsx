e32f46cab1e8619c6e7ec6af9fd0d8fbc1202ce6
bryanng412 <bng@ucdavis.edu>
2016-05-25 00:13:57 -0700

    change sample rate

--- frontend/components/visualizer.jsx
+++ frontend/components/visualizer.jsx
@@ -155,7 +155,7 @@ var Visualizer = React.createClass({
         total += this.dataArray[i];
       }
       this.volume = total;
-    }.bind(this), 50);
+    }.bind(this), 35);
     //use these in animations
     //size is fftSize/2
     this.dataArray = new window.Uint8Array(bufferLength);
