d8a6f1e4aab740c9e617da5818d47f7a63dd794d
bryanng412 <bng@ucdavis.edu>
2016-05-26 00:25:31 -0700

    remove light

--- frontend/components/visualizer.jsx
+++ frontend/components/visualizer.jsx
@@ -21,9 +21,6 @@ var Visualizer = React.createClass({
     this.renderer = new THREE.WebGLRenderer();
     document.body.appendChild(this.renderer.domElement);
 
-    this.light = new THREE.AmbientLight( 0x404040 );
-    this.scene.add(this.light);
-
     this.createWalls();
     this.createParticles();
     this.updateParticles();
@@ -107,10 +104,8 @@ var Visualizer = React.createClass({
 
       if (this.midFreq > 10000) {
         this.particles[i].intensity = 0.2 + Math.sin(this.highFreq) * Math.sin(this.highFreq);
-        this.light.intensity = 0.6;
         // this.particles[i].intensity += 0.01;
       } else {
-        this.light.intensity = 0;
         this.particles[i].intensity += 0.01;
       }
 
